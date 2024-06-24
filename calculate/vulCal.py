126199464df8e2d9c008e4b22a0ff0e537fcba4c
NUAA-YANG <2997357856@qq.com>
2024-06-24 20:02:16 +0800

    重设漏洞CVSS得分

--- calculate/vulCal.py
+++ calculate/vulCal.py
@@ -9,20 +9,21 @@ import numpy as np
 # 根据CVSS生成节点的消耗
 def calculate(vulnerability):
     # 攻击者倾向于服务复杂程度低的服务
-    complexityMap = {"low": 0.4, "medium": 0.7, "high": 0.9}
+    complexityMap = {"low": 1, "medium": 3, "high": 5}
     # 攻击者倾向于漏洞被开辟后的持续时间长的服务
-    persistenceMap = {"long": 0.4, "medium": 0.7, "short": 0.9}
+    persistenceMap = {"long": 1, "medium": 3, "short": 5}
     # 攻击者倾向于不需要交互的服务
-    interactionMap = {"no": 0.5, "yes": 0.9}
+    interactionMap = {"no": 2, "yes": 5}
     # 攻击者倾向于用户权限就可以放访问服务
-    authorityMap = {"user": 0.5, "root": 0.9}
+    authorityMap = {"user": 2, "root": 5}
     # 攻击者倾向于机密性高的服务
-    confidentialityMap = {"exHigh": 0.3, "high": 0.5, "medium": 0.7, "low": 0.9}
+    confidentialityMap = {"exHigh": 1, "high": 3, "medium": 5, "low": 7}
     # 记录每次攻击的消耗
-    vulnerability.cost = round(
-        complexityMap.get(vulnerability.complexity, 0) + persistenceMap.get(vulnerability.persistence, 0) +
-        interactionMap.get(vulnerability.interaction, 0) + authorityMap.get(vulnerability.authority, 0) +
-        confidentialityMap.get(vulnerability.confidentiality, 0), 3) * 5
+    vulnerability.vul_cost = round(
+        complexityMap.get(vulnerability.vul_complexity, 0) + persistenceMap.get(vulnerability.vul_persistence, 0) +
+        interactionMap.get(vulnerability.vul_interaction, 0) + authorityMap.get(vulnerability.vul_authority, 0) +
+        confidentialityMap.get(vulnerability.vul_confidentiality, 0), 3)
+    vulnerability.vul_reward = 40-vulnerability.vul_cost
     # 返回漏洞
     return vulnerability
 
